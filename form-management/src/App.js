bfe5dc95f440573a7c69a2dc494b28f1d059db37
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-08-09 14:12:41 -0400

    could not get class component to work with formik

--- form-management/src/App.js
+++ form-management/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import MyForm from './Components/Form';
+import FormList from './Components/FormList';
 
 
 export default class App extends Component {
@@ -8,14 +9,23 @@ export default class App extends Component {
     super(props)
   
     this.state = {
-       
+       users : []
     }
   }
+  componentDidUpdate(prevProps,prevState){
+    this.setState({
+      users: [...this.state.users,prevProps.values]
+    })
+    
+  }
+
   
   render() {
+    console.log(this.state.users)
     return (
       <div>
-        <MyForm />
+        <MyForm  />
+        <FormList />
         
       </div>
     )
